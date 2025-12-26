47064fef70bdb254b64269a4523188eca4817d81
Borja Esteban <boressan@outlook.com>
2025-12-26 19:40:34 +0100

    refactor: Update LEC import to replace LECParams with LECConfig

--- src/torch_tem/core/__init__.py
+++ src/torch_tem/core/__init__.py
@@ -1,7 +1,7 @@
 """Core components for torch_tem package."""
 
 from torch_tem.core.hpc import HPCConfig, HPCModel, HPCState
-from torch_tem.core.lec import LECModel, LECParams, LECState
+from torch_tem.core.lec import LECConfig, LECModel, LECState
 from torch_tem.core.mec import MECModel, MECParams, MECState
 from torch_tem.core.mlp import MLP
 from torch_tem.core.model import TEMModel, TEMState
@@ -14,7 +14,7 @@ __all__ = [
     "HPCConfig",
     "HPCState",
     "LECModel",
-    "LECParams",
+    "LECConfig",
     "LECState",
     "MECModel",
     "MECParams",
